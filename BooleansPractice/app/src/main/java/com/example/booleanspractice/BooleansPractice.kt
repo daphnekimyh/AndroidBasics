1d2731146405d1f48a100d8dc964011b90160850
git daphnekimyh <git daphneeun@gmail.com>
2024-06-14 21:10:54 +0900

    FloatingTypes

---
+++ BooleansPractice/app/src/main/java/com/example/booleanspractice/BooleansPractice.kt
@@ -0,0 +1,10 @@
+package com.example.booleanspractice
+
+fun main(){
+    var myTrue : Boolean = true
+    var myFalse : Boolean = false
+
+    println(myTrue || myFalse) // true
+    println(myTrue && myFalse) // false
+    println(!myTrue) // false
+}
---
+++ VariableTypes/app/src/main/java/com/example/variabletypes/VariableTypes.kt
@@ -0,0 +1,10 @@
+package com.example.variabletypes
+
+fun main(){
+    val constantNumber = 0
+    var mutableString = 1
+    println(constantNumber)
+    println(mutableString)
+    mutableString = 2
+    println(mutableString)
+}
