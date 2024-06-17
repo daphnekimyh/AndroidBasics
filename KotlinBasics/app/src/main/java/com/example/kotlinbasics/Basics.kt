3fa4e828875e307aa2a3ad3d1fe7a7cfe414dcfb
Daphne Kim <101921685+daphnekimyh@users.noreply.github.com>
2024-06-17 14:48:23 +0900

    Update Basics.kt

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -81,11 +81,11 @@ fun main(){
 // Define Function
 fun makeCoffee(sugarCount : Int, name: String){
     if(sugarCount == 1){
-        println("Coffee with $sugarCount spoon of $name")
+        println("Coffee with $sugarCount spoon of sugar for $name")
     }else if(sugarCount == 0){
         println("Coffee with no sugar for $name")
     }else{
-        println("Coffee with $sugarCount spoons of $name")
+        println("Coffee with $sugarCount spoons of sugar for $name")
     }
 
 }
